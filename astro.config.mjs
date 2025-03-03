287f1610c8352e81f54487b0204eb146c9e27e47
Kurt <kurt@homeworkclub.co>
2025-03-03 09:11:20 +0000

    add assets.tina to image optim test 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tinaDirective from "./astro-tina-directive/register"
 import netlify from '@astrojs/netlify';
+import { imageService } from '@unpic/astro/service'
+
 
 import icon from 'astro-icon';
 
@@ -12,10 +14,14 @@ import icon from 'astro-icon';
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react(), tinaDirective(), icon()],
+  image: {
+    service: imageService(),
+    domains: ['assets.tina.io'],
+  },
 
   redirects: {
-      '/admin': '/admin/index.html'
-      },
+    '/admin': '/admin/index.html',
+  },
 
-  adapter: netlify()
-});
+  adapter: netlify(),
+})
